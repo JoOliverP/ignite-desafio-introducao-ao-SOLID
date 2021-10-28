ea05c23a3f5ab432852f5c60c5f0a409b4ea5c8b
Jo Oliver <joypinheiro63@gmail.com>
2021-10-28 09:56:44 -0300

    Updated main

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -6,10 +6,19 @@ interface IRequest {
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  // eslint-disable-next-line prettier/prettier
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findById(user_id);
+
+    if (!userAlreadyExists) {
+      throw new Error("User not exists");
+    }
+
+    const admin = this.usersRepository.turnAdmin(userAlreadyExists);
+
+    return admin;
   }
 }
 
